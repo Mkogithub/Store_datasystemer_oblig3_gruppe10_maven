2e2d5ffc40ddb670bedfeff01949fe60f07ae04c
Mkogithub <97608801+Mkogithub@users.noreply.github.com>
2022-11-29 18:50:53 +0100

    New docker file and such

--- CoronaApi/src/main/java/com/example/CoronaApi/CoronaApiApplication.java
+++ CoronaApi/src/main/java/com/example/CoronaApi/CoronaApiApplication.java
@@ -12,11 +12,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Bean;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
 import java.util.Date;
